5f9a700cbbbfaf61fa436096a1ff2297424c5bb4
Heritage <ademokunwadaniel@gmail.com>
2026-06-08 19:20:12 +0100

    day3,1

---
+++ day3.py
@@ -0,0 +1,12 @@
+num_one = 5
+num_two = 10
+total = num_one+ num_two
+diff = num_two - num_one
+product = num_one*num_two
+div = num_one/num_two
+mod = num_one % num_two
+print("total:", total)
+print("diff:", diff)
+print("product:", product)
+print("div:", div)
+print("mod:", mod)
--- variable.py
+++ variable.py
@@ -8,6 +8,16 @@ age = 106
 is_married = True
 is_true = "married"
 is_light_on = True
-print(len(first_name))
-print(len(last_name))
-print(len(first_name)>len(last_name))
+#print(len(first_name))
+#print(len(last_name))
+#print(len(first_name)>len(last_name))
+num_one = 5
+num_two = 4
+num_three = num_two-num_one
+num_four = num_two*num_one
+num_five = num_one/num_two
+num_six = num_two % num_one
+num_seven = num_one** num_two
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+print(float(num_seven))
