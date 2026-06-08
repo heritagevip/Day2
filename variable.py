240d6fb4aeab87e3802ea6827640d7d2a7e3b6f1
Heritage <ademokunwadaniel@gmail.com>
2026-06-08 12:54:15 +0100

    second-commit

--- variable.py
+++ variable.py
@@ -1,6 +1,6 @@
 # day2 30 day programming 
 first_name = "Ademokunwa"
-last_nmae = "Heritage"
+last_name = "Heritage"
 full_name = "Ademokunwa_Heritage"
 country = "Nigeria"
 city = "Ekiti"
@@ -9,8 +9,5 @@ is_married = True
 is_true = "married"
 is_light_on = True
 print(len(first_name))
-print(len(last_nmae))
-print(len(full_name))
-print(len(country))
-print(len(city))
-print(len(is_true))
+print(len(last_name))
+print(len(first_name)>len(last_name))
