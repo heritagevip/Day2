d6a0ad34671d0006a3bd13c043283b32ba94fdde
Heritage <ademokunwadaniel@gmail.com>
2026-06-08 12:39:35 +0100

    first

---
+++ variable.py
@@ -0,0 +1,16 @@
+# day2 30 day programming 
+first_name = "Ademokunwa"
+last_nmae = "Heritage"
+full_name = "Ademokunwa_Heritage"
+country = "Nigeria"
+city = "Ekiti"
+age = 106
+is_married = True
+is_true = "married"
+is_light_on = True
+print(len(first_name))
+print(len(last_nmae))
+print(len(full_name))
+print(len(country))
+print(len(city))
+print(len(is_true))
