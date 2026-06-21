2cf2daa886eb085f641ada3aa5ea14d753199477
V.I.P <ademokunwadaniel@gmail.com>
2026-06-21 21:04:14 +0100

    my first commit on linu

---
+++ day4.py
@@ -0,0 +1,22 @@
+multi= """ this is a multi line code 
+it is useful for multitasking
+"""
+first_name = "Ademokunwa"
+last_name = "Heritage"
+space = " "
+full_name = first_name + space + last_name
+'''print("i hope everyone is enjoying their holiday. \n Are you ?")
+print("day \t Topic \t excercise")
+print("1 \t 5 \t 5")
+print("2 \t 6 \t 8")
+print("this is a symbol of backlash (\\)")
+print("Every programming language starts with 'Hello world!'")
+print ('i am %s %s and my fullname is %s' %(first_name, last_name, full_name))'''
+language = 'python'
+a,b,c,d,e,f = language
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
